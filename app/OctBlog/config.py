f85d3ec72c417e45fbe629d819501a996d1b2fc7
panglei <panglei@uxmc.cn>
2017-06-06 10:50:23 +0800

    图片连接修改.

--- app/OctBlog/config.py
+++ app/OctBlog/config.py
@@ -6,67 +6,6 @@ import datetime
 import os
 import sys
 
-# OctBlogSettings = {
-#     'post_types': ('post', 'page'), # deprecated
-#     'allow_registration': os.environ.get('allow_registration', 'false').lower() == 'true',
-#     'allow_su_creation': os.environ.get('allow_su_creation', 'false').lower() == 'true',
-#     'allow_donate': os.environ.get('allow_donate', 'true').lower() == 'true',
-#     'auto_role': os.environ.get('auto_role', 'reader').lower(),
-#     'blog_meta': {
-#         'name': os.environ.get('name') if os.environ.get('name') else 'Oct Blog',
-#         'subtitle': os.environ.get('subtitle') if os.environ.get('subtitle') else 'Oct Blog Subtitle',
-#         'description': os.environ.get('description') if os.environ.get('description') else 'Oct Blog Description',
-#         'wechat_name': os.environ.get('wechat_name') if os.environ.get('wechat_name') else 'Oct Blog Wechat Root',
-#         'wechat_subtitle': os.environ.get('wechat_subtitle') if os.environ.get('wechat_subtitle') else 'Oct Blog Wechat Subtitle',
-#         'owner': os.environ.get('owner') if os.environ.get('owner') else 'Gevin',
-#         'keywords': os.environ.get('keywords') if os.environ.get('keywords') else 'python,django,flask,docker,MongoDB',
-#         'google_site_verification': os.environ.get('google_site_verification') or '12345678',
-#         'baidu_site_verification': os.environ.get('baidu_site_verification') or '87654321',
-#         'sogou_site_verification': os.environ.get('sogou_site_verification') or '87654321',
-#     },
-#     'search_engine_submit_urls':{
-#         'baidu': os.environ.get('baidu_submit_url')
-#     },
-#     'pagination':{
-#         'per_page': int(os.environ.get('per_page', 5)),
-#         'admin_per_page': int(os.environ.get('admin_per_page', 10)),
-#         'archive_per_page': int(os.environ.get('admin_per_page', 20)),
-#     },
-#     'blog_comment':{
-#         'allow_comment': os.environ.get('allow_comment', 'true').lower() == 'true',
-#         'comment_type': os.environ.get('comment_type', 'octblog').lower(), # currently, OctBlog only supports duoshuo comment
-#         'comment_opt':{
-#             'octblog': 'oct-blog', # shotname of octblog
-#             'duoshuo': 'oct-blog', # shotname of duoshuo
-#             }
-#     },
-#     'donation': {
-#         'allow_donate': os.environ.get('allow_donate', 'true').lower() == 'true',
-#         'donation_msg': os.environ.get('donation_msg', 'You can donate to me if the article makes sense to you'),
-#         'donation_img_url': os.environ.get('donation_img_url') or 'http://img-own.igevin.info/weixin-pay.jpg?imageView/2/w/300'
-#     },
-#     'wechat': {
-#         'display_wechat': os.environ.get('display_wechat', 'true').lower() == 'true',
-#         'wechat_msg': os.environ.get('wechat_msg', 'Welcome to follow my wechat'),
-#         'wechat_image_url': os.environ.get('wechat_image_url') or 'http://7tsygu.com1.z0.glb.clouddn.com/gevin-view.jpg?imageView/2/w/150',
-#         'wechat_title': os.environ.get('wechat_title', 'GevinView'),
-#     },
-#     'copyright': {
-#         'display_copyright': os.environ.get('allow_display_copyright', 'true').lower() == 'true',
-#         'copyright_msg': os.environ.get('copyright_msg', 'The article is not allowed to repost unless author authorized')
-#     },
-#     'only_abstract_in_feed': os.environ.get('only_abstract_in_feed', 'false').lower() == 'true',
-#     'allow_share_article': os.environ.get('allow_share_article', 'true').lower() == 'true',
-#     'gavatar_cdn_base': os.environ.get('gavatar_cdn_base', '//cdn.v2ex.com/gravatar/'),
-#     'gavatar_default_image': os.environ.get('gavatar_default_image', 'http://7tsygu.com1.z0.glb.clouddn.com/user-avatar.jpg'),
-#     'background_image': {
-#         'home': os.environ.get('bg_home') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog-bg.jpg',
-#         'post': os.environ.get('bg_post') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog-bg.jpg',
-#         'about': os.environ.get('bg_about') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog_about.jpg',
-#         'qiniu': os.environ.get('qiniu') or 'http://assets.qiniu.com/qiniu-transparent.png',
-#     },
-
-# }
 
 if sys.version_info < (3, 0):
     OctBlogSettings = {
@@ -109,13 +48,13 @@ if sys.version_info < (3, 0):
             'donation_msg': os.environ.get('donation_msg',
                                            'You can donate to me if the article makes sense to you').decode('utf8'),
             'donation_img_url': os.environ.get(
-                'donation_img_url') or 'http://img-own.igevin.info/weixin-pay.jpg?imageView/2/w/300'
+                'donation_img_url') or 'http://or3t8xowf.bkt.clouddn.com/weixin-pay.jpg'
         },
         'wechat': {
             'display_wechat': os.environ.get('display_wechat', 'true').lower() == 'true',
             'wechat_msg': os.environ.get('wechat_msg', 'Welcome to follow my wechat').decode('utf8'),
             'wechat_image_url': os.environ.get(
-                'wechat_image_url') or 'http://7tsygu.com1.z0.glb.clouddn.com/gevin-view.jpg?imageView/2/w/150',
+                'wechat_image_url') or 'http://or3t8xowf.bkt.clouddn.com/gevin-view.jpg.jpg',
             'wechat_title': os.environ.get('wechat_title', 'GevinView'),
         },
         'copyright': {
@@ -179,13 +118,13 @@ else:
             'allow_donate': os.environ.get('allow_donate', 'true').lower() == 'true',
             'donation_msg': os.environ.get('donation_msg', 'You can donate to me if the article makes sense to you'),
             'donation_img_url': os.environ.get(
-                'donation_img_url') or 'http://img-own.igevin.info/weixin-pay.jpg?imageView/2/w/300'
+                'donation_img_url') or 'http://or3t8xowf.bkt.clouddn.com/weixin-pay.jpg'
         },
         'wechat': {
             'display_wechat': os.environ.get('display_wechat', 'true').lower() == 'true',
             'wechat_msg': os.environ.get('wechat_msg', 'Welcome to follow my wechat'),
             'wechat_image_url': os.environ.get(
-                'wechat_image_url') or 'http://or3t8xowf.bkt.clouddn.com/QQ%E5%9B%BE%E7%89%8720170606102803.jpg',
+                'wechat_image_url') or 'http://or3t8xowf.bkt.clouddn.com/gevin-view.jpg.jpg',
             'wechat_title': os.environ.get('wechat_title', 'GevinView'),
         },
         'copyright': {
